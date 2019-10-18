4171f61125a7330fab03f9f3dc75cd550e46fe76
Igor <savchenko.igor@gmail.com>
2019-10-18 16:25:32 +0700

    commit 2

--- task.py
+++ task.py
@@ -45,19 +45,34 @@ def generate_percentile_distribution(initial_collection, n_samples=None, percent
 timeseries_of_10_day_returns=calculate_n_days_returns(timeseries_of_1_day_returns,10)
 distribution_of_1_percentile=generate_percentile_distribution(timeseries_of_10_day_returns, number_of_trials=1000) 
 counter=[('median','mode','mean')]
-for i in range(1000, 20000, 1000):
+distributions=[]
+percentile_1=[]
+percentile_99=[]
+for i in range(100, 1001, 100):
     dist=generate_percentile_distribution(timeseries_of_10_day_returns, number_of_trials=i)
-    counter.append((statistics.median(dist), statistics.mode(dist), statistics.mean(dist)))
-
-print(counter)
-
+    distributions.append(dist)
+for i in range(1000, 10001, 1000):
+    dist=generate_percentile_distribution(timeseries_of_10_day_returns, number_of_trials=i)
+    distributions.append(dist)
+for i in range(10000, 100001, 10000):
+    dist=generate_percentile_distribution(timeseries_of_10_day_returns, number_of_trials=i)
+    distributions.append(dist)
+#for i in range(1000, 20000, 1000):
+    #dist=generate_percentile_distribution(timeseries_of_10_day_returns, number_of_trials=i)
+    #counter.append((statistics.median(dist), statistics.mode(dist), statistics.mean(dist)))
+    #percentile_1.append(np.percentile(dist, 1))
+    #percentile_99.append(np.percentile(dist, 99))
+    #distributions.append(dist)
 
 
 fig1, p1=plt.subplots()
 fig2, p2=plt.subplots()
 fig3, p3=plt.subplots()
+fig4, p4=plt.subplots()
 p1.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
 p1.hist(timeseries_of_1_day_returns, bins=50, density=True, histtype='stepfilled', alpha=0.3, color='green')
 p2.hist(timeseries_of_10_day_returns, bins=50, density=True, histtype='stepfilled', alpha=0.3, color='green')
 p3.hist(distribution_of_1_percentile, bins=200, density=True, histtype='stepfilled', alpha=0.3, color='green')
+for dist in distributions:
+    print(len(dist), statistics.mean(dist), statistics.median(dist), statistics.stdev(dist), statistics.variance(dist))
 plt.show()
